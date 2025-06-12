4a9cf198c06bfd8a13118f43828b062f7155d6ee
mich.kolomanski <mich.kolomanski@gmail.com>
2025-06-12 21:26:10 +0200

    fix: [#32] terms of use

--- ui/apps/ui/src/app/pages/acceptable-use-policy/acceptable-use-policy.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ess-acceptable-use-policy',
-  templateUrl: './acceptable-use-policy.component.html',
-  styleUrls: ['./acceptable-use-policy.component.scss'],
-})
-export class AcceptableUsePolicyComponent {}
---
+++ ui/apps/ui/src/app/pages/terms-of-use/terms-of-use.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ess-terms-of-use',
+  templateUrl: './terms-of-use.component.html',
+  styleUrls: ['./terms-of-use.component.scss'],
+})
+export class TermsOfUseComponent {}
