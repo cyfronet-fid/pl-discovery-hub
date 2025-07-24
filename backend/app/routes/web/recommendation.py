e68b0e013cb75b3f8db862461977bc3d373039b7
mich.kolomanski <mich.kolomanski@gmail.com>
2025-07-24 13:06:50 +0200

    fix: [#66] simplify oidc defaults

--- backend/app/routes/web/recommendation.py
+++ backend/app/routes/web/recommendation.py
@@ -66,14 +66,16 @@ async def get_recommendations(
                     uuids = await get_fixed_recommendations(panel_id, scope=scope)
                     items = await get_recommended_items(client, uuids, scope)
 
-                resp = JSONResponse({
-                    "recommendations": items,
-                    "isRand": bool(items),
-                    "message": (
-                        str(error)
-                        or "Solr or external recommender service read timeout"
-                    ),
-                })
+                resp = JSONResponse(
+                    {
+                        "recommendations": items,
+                        "isRand": bool(items),
+                        "message": (
+                            str(error)
+                            or "Solr or external recommender service read timeout"
+                        ),
+                    }
+                )
                 # We're storing the visit id for fixed recommendations just in case as well
                 resp.set_cookie("recommendation_visit_id", recommendation_visit_id)
                 return resp
--- backend/app/routes/web/search_suggestions.py
+++ backend/app/routes/web/search_suggestions.py
@@ -49,19 +49,21 @@ async def search_suggestions(
         ]
     )
 
-    gathered_result = await asyncio.gather(*[
-        _search(
-            col,
-            q,
-            qf,
-            exact,
-            fq,
-            results_per_collection,
-            search,
-            scope,
-        )
-        for col in collections
-    ])
+    gathered_result = await asyncio.gather(
+        *[
+            _search(
+                col,
+                q,
+                qf,
+                exact,
+                fq,
+                results_per_collection,
+                search,
+                scope,
+            )
+            for col in collections
+        ]
+    )
 
     return dict(gathered_result)
 
